01a75ae7c045f0efebe37a10d6dddb361364a754
pgigoux <pgigoux@gemini.edu>
2023-10-03 14:20:20 -0300

    Bug fixes and refactoring

--- centroids.py
+++ centroids.py
@@ -1,52 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from photutils.centroids import centroid_com, centroid_sources
-from photutils.datasets import make_4gaussians_image
+from photutils.centroids import centroid_sources, centroid_2dg
 from astropy.io import fits
 
 
-def open_fits_image(file_name: str) -> np.ndarray:
+def read_fits_image(file_name: str) -> np.ndarray:
+    """
+    Read the pixels from a FITS image
+    :param file_name: FITS file name
+    :return: ndarray with the pixel data
+    """
     hdu_list = fits.open(file_name)
     hdu_list.info()
     return hdu_list[0].data
 
 
-def something(data: np.ndarray):
-    # data = make_4gaussians_image()
+def sample_code(image_data: np.ndarray):
+    """
+    This is a piece of sample code largely based on the photutils.centroids documentation
+    https://photutils.readthedocs.io/en/stable/centroids.html
+    Used for initial testing
+    :param image_data: image pixel data
+    """
 
-    y_max, x_max = data.shape
-    print(x_max, y_max)
-    print(data.mean())
-
-    x1 = x_max * 0.25
-    y1 = y_max * 0.25
-    x2 = x_max * 0.75
-    y2 = y1
-    x3 = x1
-    y3 = y_max * 0.75
-    x4 = x2
-    y4 = y3
+    # Calculate initial positions
+    y_max, x_max = image_data.shape
+    x1, y1 = x_max * 0.25, y_max * 0.25
+    x2, y2 = x_max * 0.75, y1
+    x3, y3 = x1, y_max * 0.75
+    x4, y4 = x2, y3
 
     x_init = (x1, x2, x3, x4)
     y_init = (y1, y2, y3, y4)
 
-    print(x_init, y_init)
-    x, y = centroid_sources(data, x_init, y_init, box_size=101,
-                            centroid_func=centroid_com)
+    # Calculate the centroids
+    x, y = centroid_sources(image_data, x_init, y_init, box_size=201,
+                            centroid_func=centroid_2dg)
 
-    # x, y = centroid_com(data[0:int(x_max/2), 0:int(y_max/2)])
-    print(x, y)
-
+    # Plot the results
     plt.figure(figsize=(8, 4))
-    plt.imshow(data, origin='lower', interpolation='nearest', cmap='gray')
-    plt.scatter(x_init, y_init, marker='+', s=80, color='red', label='Centroids')
+    plt.imshow(image_data, origin='lower', interpolation='nearest', cmap='gray')
+    plt.scatter(x, y, marker='+', s=80, color='red', label='Centroids')
     plt.legend()
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == '__main__':
-    image = open_fits_image('test.fits')
-    something(image)
-
-
+    image = read_fits_image('test1.fits')
+    sample_code(image)
